5e7f5486584f4f58401bc9df8c57227a31db032c
a1175643811-bit <a1175643811@gmail.com>
2026-05-04 00:30:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const rule = {title:"🔞成人源",version:"1.0",author:"G",sites:[{name:"🔞滴滴",api:"https://api.ddapi.cc/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞鸡坤",api:"https://jkunzyapi.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞TG资源",api:"https://tgzyz.pp.ua/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞奥斯卡",api:"https://aosikazy4.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞X细胞",api:"https://www.xxibaozyw.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞大奶子",api:"https://apidanaizi.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true}]};
+const rule = {
+  title: "🔞 新成人源合集",
+  version: "1.3",
+  author: "G",
+  sites: [
+    { name: "🔞滴滴新", api: "https://api.ddapi.cc/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞乐播资源", api: "https://lbapi9.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞奶香香", api: "https://naixxzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞森林资源", api: "http://slapibf.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞番茄资源", api: "https://fqzy.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞鲨鱼资源", api: "https://shayuapi.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞91麻豆", api: "http://91md.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🔞桃花资源", api: "https://thzy1.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true }
+  ]
+};
+
 module.exports = rule;

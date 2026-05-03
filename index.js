09b484b5a7cf317ec8db57a12fe2dd1b1ed7310c
a1175643811-bit <a1175643811@gmail.com>
2026-05-04 00:32:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const rule = {
-  title: "🔞 新成人源合集",
-  version: "1.3",
-  author: "G",
+  title: "🔞 测试源",
+  version: "1.0",
+  author: "Test",
   sites: [
-    { name: "🔞滴滴新", api: "https://api.ddapi.cc/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞乐播资源", api: "https://lbapi9.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞奶香香", api: "https://naixxzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞森林资源", api: "http://slapibf.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞番茄资源", api: "https://fqzy.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞鲨鱼资源", api: "https://shayuapi.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞91麻豆", api: "http://91md.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞桃花资源", api: "https://thzy1.me/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true }
+    { 
+      name: "🔞滴滴测试", 
+      api: "https://api.ddapi.cc/api.php/provide/vod", 
+      type: 3, 
+      searchable: true, 
+      quickSearch: true, 
+      filterable: false   // 关闭筛选，减少卡顿
+    }
   ]
 };
 

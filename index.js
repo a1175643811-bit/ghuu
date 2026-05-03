bcef91e63610fca3bf5c3a31e989116931fa1281
a1175643811-bit <a1175643811@gmail.com>
2026-05-04 00:33:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const rule = {
-  title: "🔞 测试源",
+  title: "📺 常用影视源",
   version: "1.0",
-  author: "Test",
+  author: "G",
   sites: [
-    { 
-      name: "🔞滴滴测试", 
-      api: "https://api.ddapi.cc/api.php/provide/vod", 
-      type: 3, 
-      searchable: true, 
-      quickSearch: true, 
-      filterable: false   // 关闭筛选，减少卡顿
-    }
+    { name: "🔥电影猫", api: "https://dydy.tv/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🍿OK资源", api: "https://okzy.tv/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🌟速看资源", api: "https://sukzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🎬阿里资源", api: "https://alizy.vip/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "📀1080资源", api: "https://1080zy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🌐无尽资源", api: "https://wujinzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
+    { name: "🐱猫猫资源", api: "https://miaozy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true }
   ]
 };
 

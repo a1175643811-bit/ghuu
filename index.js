04d1e1ddff5d48073f1d740cf89ee080009c3ead
a1175643811-bit <a1175643811@gmail.com>
2026-05-04 00:25:13 +0800

    Update rule object with new properties and values

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-const rule = {
-  title: "🔞 成人源合集",
-  version: "1.2",
-  author: "Custom",
-  sites: [
-    { name: "🔞滴滴资源", api: "https://api.ddapi.cc/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞鸡坤资源", api: "https://jkunzyapi.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞TG资源", api: "https://tgzyz.pp.ua/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞奥斯卡", api: "https://aosikazy4.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞X细胞", api: "https://www.xxibaozyw.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞大奶子", api: "https://apidanaizi.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞老色p", api: "https://apilsbzy1.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞番号资源", api: "http://fhapi9.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🔞黄色仓库", api: "https://hsckzy888.com/api.php/provide/vod/from/hsckm3u8/at/json", type: 3, searchable: true, quickSearch: true, filterable: false },
-    { name: "🔞杏吧资源", api: "https://xingba111.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true }
-  ]
-};
-
+const rule = {title:"🔞成人源",version:"1.0",author:"G",sites:[{name:"🔞滴滴",api:"https://api.ddapi.cc/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞鸡坤",api:"https://jkunzyapi.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞TG资源",api:"https://tgzyz.pp.ua/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞奥斯卡",api:"https://aosikazy4.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞X细胞",api:"https://www.xxibaozyw.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true},{name:"🔞大奶子",api:"https://apidanaizi.com/api.php/provide/vod",type:3,searchable:true,quickSearch:true,filterable:true}]};
 module.exports = rule;

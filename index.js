6024e6a48d159146827d276b80d09a1f3f7955c3
a1175643811-bit <a1175643811@gmail.com>
2026-05-04 00:56:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
+// 成人专用猫源 - 基于 stymei 格式
+const sites = [
+  {
+    name: "🔞滴滴成人",
+    api: "https://api.ddapi.cc/api.php/provide/vod",
+    type: 3,
+    searchable: true,
+    quickSearch: true,
+    filterable: true
+  },
+  {
+    name: "🔞鸡坤成人",
+    api: "https://jkunzyapi.com/api.php/provide/vod",
+    type: 3,
+    searchable: true,
+    quickSearch: true,
+    filterable: true
+  },
+  {
+    name: "🔞奥斯卡成人",
+    api: "https://aosikazy4.com/api.php/provide/vod",
+    type: 3,
+    searchable: true,
+    quickSearch: true,
+    filterable: true
+  },
+  {
+    name: "🔞X细胞成人",
+    api: "https://www.xxibaozyw.com/api.php/provide/vod",
+    type: 3,
+    searchable: true,
+    quickSearch: true,
+    filterable: true
+  },
+  {
+    name: "🔞杏吧成人",
+    api: "https://xingba111.com/api.php/provide/vod",
+    type: 3,
+    searchable: true,
+    quickSearch: true,
+    filterable: true
+  }
+];
+
 const rule = {
-  title: "📺 常用影视源",
+  title: "🔞 成人专用源",
   version: "1.0",
-  author: "G",
-  sites: [
-    { name: "🔥电影猫", api: "https://dydy.tv/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🍿OK资源", api: "https://okzy.tv/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🌟速看资源", api: "https://sukzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🎬阿里资源", api: "https://alizy.vip/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "📀1080资源", api: "https://1080zy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🌐无尽资源", api: "https://wujinzy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true },
-    { name: "🐱猫猫资源", api: "https://miaozy.com/api.php/provide/vod", type: 3, searchable: true, quickSearch: true, filterable: true }
-  ]
+  author: "Custom",
+  sites: sites
 };
 
 module.exports = rule;
